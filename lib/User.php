33dc00ca3bbfa6e3b79142ce7dc724d51d305937
amigobv <daniell.rotaru@gmail.com>
2015-07-22 23:01:25 +0200

    User class

---
+++ lib/User.php
@@ -0,0 +1,46 @@
+<?php
+
+class User extends Entity {
+	private $firstName;
+	private $lastName;
+	private $username;
+	private $hashPassword;
+	
+	public function __construct($id, $firstName, $lastName, $username, $hashPassword) {
+		parent::__construct($id);
+		$this->firstName = $firstName;
+		$this->lastName = $lastName;
+		$this->username = $username;
+		$this->hashPassword = $hashPassword;
+	}
+	
+	public function getUsername() {
+		return $this->username;
+	}
+	
+	public function setUsername($username) {
+		$this->username = $username;
+	}
+		
+	public function getPassword() {
+		return $this->password;
+	}
+	
+	public function setPassword($hashPassword) {
+		$this->hashPassword = $hashPassword;
+	}
+	
+	public function getFirstName() {
+		return $this->firstName;
+	}
+	
+	public function getLastName() {
+		return $this->lastName;
+	}
+	
+	public function setLastName($lastName) {
+		$this->lastName = $lastName;
+	}
+}
+
+?>
---
+++ lib/data/DataManager_mock.php
@@ -0,0 +1,38 @@
+<?php
+
+$__users = array(1 => new User(1, "Test", "User", "scm4", "a8af855d47d091f0376664fe588207f334cdad22"),
+				2 => new User(2, "Daniel", "Rotaru", "guest", "guest"),
+				3 => new User(3, "Robert", "Koeppe", "koeppe", "koeppe"),
+				4 => new User(4, "Christian", "Gesswagner", "gesswagner", "gesswagner"),	
+				5 => new User(5, "Manuell", "Buchinger", "buchinger", "buchinger")
+);
+
+class DataManager extends DataObject {
+	public static function getUserById($id) {
+		global $__users;
+		
+		foreach($__users as $user) {
+			if ($user->getId() == $id) {
+				return $user;
+			}
+		}
+		
+		return null;
+	}
+	
+	public static function getUserByUsername($username) {
+		global $__users;
+		
+		foreach($__users as $user) {
+			if ($user->getUsername() == $username) {
+				return $user;
+			}
+		}
+		
+		return null;
+	}
+	
+}
+
+
+
