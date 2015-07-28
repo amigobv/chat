76a126f7a294d17d53fdb516e728d009abab8448
amigobv <daniell.rotaru@gmail.com>
2015-07-28 14:56:52 +0200

    add controlle class

---
+++ lib/Controller.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dro
+ * Date: 28.07.2015
+ * Time: 14:22
+ */
+class Controller extends BaseObject {
+
+    const REQUEST_METHOD = POST;
+    const ACTION_PARAM = 'action';
+
+    const PAGE = 'page';
+
+    const ACTION_LOGIN = 'login';
+    const ACTION_LOGOUT = 'logout';
+
+    const USR_NAME = 'username';
+    const USR_PASSWORD = 'password';
+
+    private static $instance = false;
+
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new Controller();
+        }
+
+        return self::$instance;
+    }
+
+    private function __construct() {
+
+    }
+
+    public function invokePostAction() {
+        if ($_SERVER['REQUEST_METHOD'] != self::REQUEST_METHOD) {
+            throw new Exception("Controller can only handle " . self::REQUEST_METHOD . ' requests');
+            return null;
+        } else if (!isset($_REQUEST[self::ACTION_PARAM])) {
+            throw new Exceptioon(self::ACTION_PARAM . ' parameter is not specified');
+        }
+
+        $action = $_REQUEST[self::ACTION_PARAM];
+
+        switch($action) {
+            case self::ACTION_LOGIN:
+                if (!AuthenticationManager::authenticate($_REQUEST[self::USR_NAME], $_REQUEST[self::USR_PASSWORD])) {
+                    $this->forwardRequest(['Invalid user information provided']);
+                }
+                Util::redirect();
+                break;
+
+            case self::ACTION_LOGOUT:
+                if (AuthenticationManager::isAuthenticated()) {
+                    AuthenticationManager::signOut();
+                }
+                Util::redirect();
+                break;
+        }
+    }
+
+    public function forwardRequest(array $errors = null, $target = null) {
+        if ($target == null) {
+            if (!isset($_REQUEST[self::PAGE])) {
+                throw new Exception("Missing target to forward");
+            }
+            $target = $_REQUEST[self::PAGE];
+        }
+
+        if (count($errors) > 0) {
+            $target .= '$errors=' . urlenconde(serialize($errors));
+        }
+        heder('location: ' . $target);
+        exit();
+    }
+}
