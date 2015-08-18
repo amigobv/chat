72706c55a129b1ebe02d91bfc3dcfd7dd54fa2fb
amigobv <daniell.rotaru@gmail.com>
2015-08-18 23:01:43 +0200

    chat post

---
+++ tests/PostTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Blade
+ * Date: 18.08.2015
+ * Time: 22:10
+ */
+include_once("../lib/BaseObject.php");
+include_once("../lib/Entity.php");
+include_once("../lib/User.php");
+include_once("../lib/Channel.php");
+include_once("../lib/Post.php");
+
+$user = new User(0, "Daniel", "Rotaru", "RoDa", hash('sha1', "RoDa" . '|' . '1310307036'));
+
+$channel = new Channel(0, "default");
+$post1 = new Post(0, $channel->getID(), $user->getUsername(), "My first test", "Test Test Test!!!", false);
+$post2 = new Post(1, $channel->getID(), $user->getUsername(), "My second test", "Test Test Test!!!", true);
+
+echo $post1->getID() . " " . "Channel: " . $post1->getChannelId() . " " . $post1->getAuthor() .
+    " " . $post1->getTitle() . " " . $post1->getContent() .
+    " " . $post1->getStatus() . " " . (($post1->getProminence()) ? "Important" : "Normal") . "<br>";
+
+$post1->setRead();
+$post1->setProminence(true);
+
+echo $post1->getID() . " " . "Channel: " . $post1->getChannelId() . " " . $post1->getAuthor() .
+    " " . $post1->getTitle() . " " . $post1->getContent() .
+    " " . $post1->getStatus() . " " . (($post1->getProminence()) ? "Important" : "Normal") . "<br>";
+?>
+
+
+
